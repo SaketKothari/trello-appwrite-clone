859856723275ccef27e0243aaeea0f1dc583879a
Saket Kothari <saketkothari722@gmail.com>
2023-05-28 16:42:51 +0530

    Implemented the React Avatar Library

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
+'use client';
+
 import Image from 'next/image';
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import Avatar from 'react-avatar';
 
 function Header() {
   return (
     <header>
-      <div className='flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl'>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="Trello logo"
@@ -13,7 +16,7 @@ function Header() {
           className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
         />
 
-        <div>
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           {/* Search Box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
@@ -28,6 +31,7 @@ function Header() {
           </form>
 
           {/* Avatar */}
+          <Avatar name="Saket Kothari" round size="50" color="#0055D1" />
         </div>
       </div>
     </header>
