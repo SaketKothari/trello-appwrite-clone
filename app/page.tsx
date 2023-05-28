4acd886173e32a9a165d63ece0079b21825820aa
Saket Kothari <saketkothari722@gmail.com>
2023-05-28 16:06:39 +0530

    Building the Header Component - Image & Search Box

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
     <main>
+      {/* Header */}
+      <Header />
+
+      {/* Board */}
       <h1>Trello AI Clone</h1>
     </main>
   );
